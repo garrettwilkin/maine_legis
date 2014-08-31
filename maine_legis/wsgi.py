26f695d6816b8953479297cb83a59639950be7f9
Garrett Wilkin <garrett.wilkin@gmail.com>
2014-08-31 19:52:07 +0000

    Initial project files created by django-admin.

---
+++ maine_legis/wsgi.py
@@ -0,0 +1,14 @@
+"""
+WSGI config for maine_legis project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
+"""
+
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "maine_legis.settings")
+
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
