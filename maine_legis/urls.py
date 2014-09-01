a70c9fd0156bd77ea5382eed2361f6c685299ff8
Garrett Wilkin <garrett.wilkin@gmail.com>
2014-09-01 17:28:18 +0000

    created laws app.

--- maine_legis/urls.py
+++ maine_legis/urls.py
@@ -4,9 +4,6 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'maine_legis.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
+    url(r'laws/', include('laws.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
